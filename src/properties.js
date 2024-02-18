5f63bd8385d663564aeb0732a833f286f3d9eee6
williamsilvacastro <87095450+williamsilvacastro@users.noreply.github.com>
2024-02-18 16:59:11 -0300

    adicionando build

--- src/properties.js
+++
@@ -1,7 +0,0 @@
-const properties = {
-    backendUrl: "http://emp-app-env.eba-wjjpw6cs.us-east-2.elasticbeanstalk.com"
-    // Adicione outras propriedades conforme necessário
-  };
-  
-  export default properties;
-  
